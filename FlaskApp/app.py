988a2254967a6bd5c39d5c264d929b8dd0213b79
Waleed Ahmed <wahmedwd3@gmail.com>
2020-01-18 08:25:50 -0500

    Added functions to create and upload files to a gcp bucket

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -5,13 +5,22 @@ import sys
 # Initialize flask app
 app = Flask(__name__)
 
-def upload_file(bucket_name, source_file_name, destination_blob_name):
+# Instantiate Google Cloud client
+storage_client = storage.Client()
+
+def create_bucket_gcp(bucket_name):
+    """Creates a new bucket. Can fail if bucket name is not available"""
+    # bucket_name = "your-bucket-name"
+
+    storage_client.create_bucket(bucket_name)
+    print("Bucket {} created".format(bucket_name))
+
+def upload_file_gcp(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
     # bucket_name = "your-bucket-name"
     # source_file_name = "local/path/to/file"
     # destination_blob_name = "storage-object-name"
 
-    storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
@@ -24,7 +33,8 @@ def upload_file(bucket_name, source_file_name, destination_blob_name):
 # Homepage URL routing
 @app.route('/', methods=['GET'])
 def index():
-    upload_file
+    upload_file_gcp("uofthacksvii", "test_img.png", "upload_img.png")
+
     return "Do you wanna make an app?"
 
 if __name__ == "__main__":
