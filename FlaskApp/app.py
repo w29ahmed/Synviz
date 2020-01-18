46edc223997cf1313c46274a199c60a2c2f7ded5
Waleed Ahmed <wahmedwd3@gmail.com>
2020-01-18 01:36:12 -0500

    Added function to upload file to gcp

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,12 +1,31 @@
 from flask import Flask
+from google.cloud import storage
+import sys
 
 # Initialize flask app
 app = Flask(__name__)
 
+def upload_file(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # bucket_name = "your-bucket-name"
+    # source_file_name = "local/path/to/file"
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print("File {} uploaded to {}."
+    .format(source_file_name, destination_blob_name),
+    file = sys.stderr)
+
 # Homepage URL routing
 @app.route('/', methods=['GET'])
 def index():
-    return "Hello World"
+    upload_file
+    return "Do you wanna make an app?"
 
 if __name__ == "__main__":
     app.run(debug=True)
